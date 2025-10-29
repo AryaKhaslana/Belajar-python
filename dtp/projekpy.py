16eaff7e23bb54b890454b00dbda93f023d233d3
arya <aryachakabrajawisesa@gmail.com>
2025-10-29 08:54:54 +0700

    def

--- dtp/projekpy.py
+++ dtp/projekpy.py
@@ -1,7 +1,14 @@
+
+def __init__ (nama, hobi, tujuan):
+    nama_anda = nama
+    hobi_anda = hobi
+    tujuan_anda = tujuan
+
+
+
 nama = input("Masukkan nama anda :")
 
 if nama == "aryaror":
     print("nama anda valid")
 else:
     print("nama anda tidak valid")
-
